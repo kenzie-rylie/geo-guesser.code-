9b2d2f3d05888cbe53f042ea0c03a9251e4a9402
Bob1437 <72892023+Bob1437@users.noreply.github.com>
2020-11-04 14:33:53 -0800

    Add files via upload

--- Data.py
+++ Data.py
@@ -15,5 +15,19 @@ def inputdata2(): #Second test to see if multiple datas woild would work
 def picran(): # /static/ + random number + .jpg so it would call a random jpg file because I named them all number.jpg and thet are in the static folder
     picture = int(randrange(6))
     picture2 = "/static/" + str(picture) + ".jpg"
-    dictionary2 = {"picture": picture2}
+    if picture == 1:
+        repel1 = "https://picture1website1.jhunt11111.repl.run"
+    elif picture == 5:
+        repel1 = "https://picture5website1.jhunt11111.repl.run"
+    elif picture == 3:
+        repel1 = "https://picture3website1.jhunt11111.repl.run"
+    elif picture == 0:
+        repel1 = "https://picture0website1.jhunt11111.repl.run"
+    elif picture == 2:
+        repel1 = "https://picture2website1.jhunt11111.repl.run"
+    elif picture == 4:
+        repel1 = "https://picture4website1.jhunt11111.repl.run"
+    else:
+        repel1 = str(picture)
+    dictionary2 = {"picture": picture2, "repel1": repel1}
     return dictionary2
